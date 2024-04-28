a820452881eb1bcf529709130fb823caae599546
setiaaa <setiaaaya@gmail.com>
2024-04-28 12:55:09 +0700

    Add SB Admin Bootstrap

--- resources/views/Contents/login.blade.php
+++ resources/views/Contents/login.blade.php
@@ -1,27 +1,51 @@
 @extends('Layouts.layout')
 
 @section('content')
-    <section>
-        <div class="row">
-            <div class="col align-self-center display-flex">
-                <div class="container mx-3">
-                    <form action="" method="get">
-                        <div class="input-group mb-3">
-                            <span class="input-group-text" id="basic-addon1">@</span>
-                            <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
+            <div class="col-xl-10 col-lg-12 col-md-9">
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-6">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                    </div>
+                                    <form class="user">
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail">Masukan username alamat emailmu</label>
+                                            <input type="email" class="form-control form-control-user"
+                                                id="exampleInputEmail" aria-describedby="emailHelp"
+                                                placeholder="username_ atau nama@contoh.com">
+                                        </div>
+                                        <div class="form-group">
+                                            <label for="exampleInputPassword">Masukan kata sandimu</label>
+                                            <input type="password" class="form-control form-control-user"
+                                                id="exampleInputPassword" placeholder="Paling tidak 8 karakter">
+                                        </div>
+                                        <a href="index.html" class="btn Primary-70 btn-block">
+                                            Masuk
+                                        </a>
+                                    </form>
+                                </div>
+                            </div>
                         </div>
-                        <img src="" alt="">
-                    </form>
+                    </div>
                 </div>
             </div>
-            <div class="col d-none d-sm-block">
-                <form action="" method="get">
-                    <div class="input-group mb-3">
-                        <span class="input-group-text" id="basic-addon1">@</span>
-                        <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-                    </div>
-                </form>
-            </div>    
         </div>
-    </section>
+
+    {{-- Bootsrap core JavaScript --}}
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="js/bootstrap/sb-admin-2.min.js"></script>
 @endsection
--- resources/views/Layouts/layout.blade.php
+++ resources/views/Layouts/layout.blade.php
@@ -6,6 +6,13 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Sistem Pencatatan - SPK</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    
+    <!-- Custom fonts for this template-->
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
+    <link rel="stylesheet" href="css/bootstrap/sb-admin-2.min.css">
   </head>
 </head>
 <body>
